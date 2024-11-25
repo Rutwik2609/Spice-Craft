7f0aa4a5e05b9627d607e8c62ac2059dfb923c06
procoder2609 <rutwika.872000@gmail.com>
2024-11-25 12:24:46 +0530

    Final Updates

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,35 +1,27 @@
-import React from 'react'
-import RecipeCard from '../components/RecipeCard';
+import RecipeCard from "../components/RecipeCard";
+import { getRandomColor } from "../lib/utils";
 
 const FavoritesPage = () => {
+	const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
-  const fav=false;
+	return (
+		<div className='flex-1 p-10 min-h-screen'>
+			<div className='max-w-screen-lg mx-auto'>
+				<p className='font-bold text-3xl md:text-5xl my-4'>My Favorites</p>
 
-  return (
-    <div className='flex-1 p-8 min-h-screen '>
-      <div className='max-w-screen-lg mx-auto'  >
-        <p className='text-3xl font-bold md:text-5xl my-4'>Favourites</p>
+				{favorites.length === 0 && (
+					<div className='h-[80vh] flex flex-col items-center gap-4'>
+						<img src='/404.svg' className='h-3/4' alt='404 svg' />
+					</div>
+				)}
 
-        {!fav && (
-          <div className='h-[80vh] flex flex-col justify-center items-center gap-4' >
-            <img src="/404.svg" alt="PNF" className='h-3/4 object-contain ' />
-            <h1 className='font-bold tracking-tighter'>No Favourite Recipes</h1>
-          </div>
-        )}
-
-
-        {fav &&(
-          <div className='grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-            <RecipeCard/>
-            <RecipeCard/>
-            <RecipeCard/>
-
-          </div>
-        )}
-      </div>
-      
-    </div>
-  )
-}
-
-export default FavoritesPage
+				<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+					{favorites.map((recipe) => (
+						<RecipeCard key={recipe.label} recipe={recipe} {...getRandomColor()} />
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
+export default FavoritesPage;
